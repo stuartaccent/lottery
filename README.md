7f811ff815888e9f13b83a6277429badc93d2418
Stuart George <stuart@accentdesign.co.uk>
2024-02-21 18:58:29 +0000

    1 to 59

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Lottery Number Generator
 
-Draws 6 numbers from a range of 49 numbers, 1 to 49, and runs the simulation 1,000,000 times
+Draws 6 numbers from a range of 59 numbers, 1 to 59, and runs the simulation 1,000,000 times
 to find the most frequent lottery numbers sets.
 
 * The top 20 most frequent lottery numbers sets are printed.
--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type (
 	}
 )
 
-var allNumbers = make([]int, 49)
+var allNumbers = make([]int, 59)
 
 func drawLottery(rng *rand.Rand) []int {
 	rng.Shuffle(len(allNumbers), func(i, j int) {
@@ -81,13 +81,13 @@ func timeTaken(start time.Time, name string) {
 	fmt.Printf("%s took %s\n", name, elapsed)
 }
 
-// Draws 6 numbers from a range of 49 numbers, 1 to 49, and runs the simulation 1,000,000 times
+// Draws 6 numbers from a range of 59 numbers, 1 to 59, and runs the simulation 1,000,000 times
 // to find the most frequent lottery numbers sets.
 // The top 20 most frequent lottery numbers sets are printed.
 // The top 20 most frequent drawn individual numbers are printed.
 func main() {
 	defer timeTaken(time.Now(), "runSimulation")
-	for i := range 49 {
+	for i := range 59 {
 		allNumbers[i] = i + 1
 	}
 	runSimulation(1000000, 20)
